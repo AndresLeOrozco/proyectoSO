3839dd3dfe61074e12dac4d5d793015eaf3b45eb
andre <andres.leon.orozco@est.una.ac.cr>
2023-06-17 00:29:54 -0600

    inicio gui

--- GUI.py
+++ GUI.py
@@ -1,38 +1,48 @@
 import tkinter as tk
-from tkinter import messagebox, filedialog
+from tkinter import *
 from CPUsimulation import *
 
 class GUI:
     def __init__(self, root):
         self.root = root
+        self.root.geometry("500x500")
         self.root.title("Simulación de Planificación de CPU")
-        self.simulation = CPUSimulation()
         self.selected_algorithm = tk.StringVar()  # Variable de control para el algoritmo seleccionado
 
         # Crear widgets
         self.label_algorithm = tk.Label(root, text="Seleccione el algoritmo:")
         self.combo_algorithm = tk.OptionMenu(root, self.selected_algorithm, *["Round Robin", "Prioridades con Round Robin"])
-        self.btn_load_processes = tk.Button(root, text="Cargar procesos", command=self.load_processes)
-        self.btn_start_simulation = tk.Button(root, text="Iniciar simulación", command=self.start_simulation)
-        self.btn_interrupt = tk.Button(root, text="Interrupción de E/S", command=self.io_interrupt)
-        self.btn_finish_simulation = tk.Button(root, text="Finalizar simulación", command=self.finish_simulation)
+        self.btn_start_simulation = tk.Button(root, text="Iniciar simulación", command=lambda:self.start_simulation())
+        self.label_quantity = tk.Label(root, text="Ingrese la cantidad de procesos en la simulacion:")
+        self.process_quantity =  tk.Entry(root)
+        self.label_quantum = tk.Label(root, text="Ingrese el tiempo del quantum:")
+        self.process_quantum = tk.Entry(root)
 
         # Posicionar widgets
         self.label_algorithm.pack()
         self.combo_algorithm.pack()
-        self.btn_load_processes.pack()
-        self.btn_start_simulation.pack()
-        self.btn_interrupt.pack()
-        self.btn_finish_simulation.pack()
 
-    
+        self.btn_start_simulation.pack()
+        self.label_quantity.pack()
+        self.process_quantity.pack()
+        self.label_quantum.pack()
+        self.process_quantum.pack()
 
     def start_simulation(self):
-        algorithm = self.selected_algorithm.get()
-        if not algorithm:
-            messagebox.showerror("Error", "Debe seleccionar un algoritmo")
+        if self.process_quantum.get() or self.process_quantity.get() == "":
+            
             return
-        self.simulation.schedule_processes(algorithm)
-        self.simulation.calculate_waiting_time()
-        messagebox.showinfo("Información", "Simulación finalizada")
+        else:  
+            simulacion = CPUSimulation(self.process_quantum.get())
+            simulacion.generate_process(self.process_quantity.get())
+            algorithm = self.selected_algorithm.get()
+            if not algorithm:
+                messagebox.showerror("Error", "Debe seleccionar un algoritmo")
+                return
+            if algorithm == "Round Robin":
+                simulacion.round_robin()
+            if algorithm == "Prioridades con Round Robin":
+                simulacion.priority_round_robin()
+            self.simulation.calculate_waiting_time()
+            messagebox.showinfo("Información", "Simulación finalizada")
 
--- proyectoSo.py
+++ proyectoSo.py
@@ -3,12 +3,8 @@ import tkinter as tk
 from CPUsimulation import *
 
 if __name__ == '__main__':
-    simulacion = CPUSimulation(1)
-    simulacion.generate_process(12)
-    #simulacion.round_robin()
-    simulacion.priority_round_robin()
-    for i in simulacion.log:
-        print(i)
-    simulacion.geStaticts()
+    root = tk.Tk()
+    gui = GUI(root)
+    root.mainloop()
 
 
